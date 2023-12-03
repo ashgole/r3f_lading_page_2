fa2cad9c5d739a595de589c26d988bc596bb4f77
ashabb <ashishgole99955@gmail.com>
2023-12-03 17:46:58 +0530

    css changed

--- src/component/gallery/Gallery.js
+++ src/component/gallery/Gallery.js
@@ -30,16 +30,19 @@ export default function Gallery() {
   /*-------------div-------------*/
 
   return <>
-    <div className="gallaryContainer flex flex-wrap gap-1.5 font-semibold">
+    <div className="  flex flex-wrap font-semibold">
       {imageArray.map((item, index) => {
-        return <div
-          key={index}
-          className="galleryImage"
-          onClick={() => selectItem(index)}
-          style={index === isHovered ? cardStyle : cardStyle2}
-          onMouseEnter={() => handleMouseEnter(index)}
-          onMouseLeave={handleMouseLeave}
-        >{item}</div>;
+        return <div className="w-full md:w-1/2 lg:w-1/3 h-64 p-2">
+          <div
+            key={index}
+            className="galleryImage h-full w-full"
+            onClick={() => selectItem(index)}
+            style={index === isHovered ? cardStyle : cardStyle2}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+          >{item}</div>
+        </div>
+
       })}
     </div>
     {isModalOn && <ImageModal setIsModalOn={setIsModalOn} selectedItem={selectedItem} />}
